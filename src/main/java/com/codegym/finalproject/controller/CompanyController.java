a298f2cdbe46fcfa11a6e83581a064036fdcf16e
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-06-12 18:05:16 +0700

    test CV function

--- src/main/java/com/codegym/finalproject/controller/CompanyController.java
+++ src/main/java/com/codegym/finalproject/controller/CompanyController.java
@@ -1,17 +1,17 @@
 package com.codegym.finalproject.controller;
 
-import com.codegym.finalproject.model.dto.CompanyForm;
 import com.codegym.finalproject.model.dto.request.EditCompany;
 import com.codegym.finalproject.model.dto.request.StatusRequest;
+import com.codegym.finalproject.model.dto.response.CompanyRecruitmentNeed;
 import com.codegym.finalproject.model.dto.response.ResponseMessage;
 import com.codegym.finalproject.model.entity.*;
 import com.codegym.finalproject.security.userprincipal.UserDetailServices;
-import com.codegym.finalproject.service.account.IAccountService;
-import com.codegym.finalproject.service.city.ICityService;
-import com.codegym.finalproject.service.company.ICompanyService;
-import com.codegym.finalproject.service.recuitmentNew.IRecuitmentNewService;
+import com.codegym.finalproject.service.account.AccountService;
+import com.codegym.finalproject.service.city.CityService;
+import com.codegym.finalproject.service.company.CompanyService;
+import com.codegym.finalproject.service.email.EmailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,58 +20,54 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
+@RequestMapping("company")
 @RestController
-@CrossOrigin("*")
-@RequestMapping("/company")
 public class CompanyController {
     @Autowired
-    private ICompanyService companyService;
-
+    CompanyService companyService;
     @Autowired
     UserDetailServices userDetailServices;
 
     @Autowired
-    private ICityService cityService;
+    CityService cityService;
 
     @Autowired
-    private IAccountService accountService;
+    AccountService accountService;
 
     @Autowired
-    private IRecuitmentNewService recuitmentNewService;
+    EmailServiceImpl emailService;
 
-    @Autowired
-    Environment env;
+    @Value("${spring.mail.username}")
+    private String from;
 
-    @ModelAttribute("city")
-    public Iterable<City> cities(){
-        return cityService.findAll();
-    }
 
-    @ModelAttribute("account")
-    public Iterable<Account> accounts() {
-        return accountService.findAll();
-    }
-
-    @ModelAttribute("recuitmentNew")
-    public Iterable<RecuitmentNew> recuitmentNews(){
-        return recuitmentNewService.findAll();
-    }
-
-    @GetMapping
-    public ResponseEntity<Iterable<Company>> findAll() {
-        Iterable<Company> companies = companyService.findAll();
-        return new ResponseEntity<>(companies, HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Company> findById(@PathVariable Long id) {
-        Optional<Company> companyOptional = companyService.findById(id);
-        if (!companyOptional.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @PostMapping("")
+    public ResponseEntity<?> createCompany(@RequestBody Company company) {
+        if (companyService.existByName(company.getName())) {
+            return new ResponseEntity<>(new ResponseMessage("no_name_category"), HttpStatus.OK);
         }
-        return new ResponseEntity<>(companyOptional.get(), HttpStatus.OK);
+        //tao codeCompany
+        String nameex = company.getName().substring(0, 3);
+        int min = 1000;
+        int max = 9999;
+        String codeCompany = String.valueOf((int) Math.floor(Math.round((Math.random() * (max - min + 1) + min))));
+        System.out.println(codeCompany);
+        company.setCodeCompany(nameex + company.getAccount().getId() + codeCompany);
+        //
+        company.setStatusCompany(Status.HOT);
+        if (company.getAvatar() == null) {
+            return new ResponseEntity<>(new ResponseMessage("no_avatar_category"), HttpStatus.OK);
+        }
+        companyService.save(company);
+        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<?> showListCompany() {
+        Iterable<Company> companyList = companyService.findAll();
+        return new ResponseEntity<>(companyList, HttpStatus.OK);
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCompany(@PathVariable Long id, @RequestBody EditCompany editCompany) {
@@ -123,51 +119,6 @@ public class CompanyController {
         return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
     }
 
-    @PostMapping("")
-    public ResponseEntity<?> createCompany(@RequestBody Company company) {
-        if (companyService.existByName(company.getName())) {
-            return new ResponseEntity<>(new ResponseMessage("this name is exist"), HttpStatus.OK);
-        }
-        //tao codeCompany
-        String nameex = company.getName().substring(0, 3);
-        int min = 1000;
-        int max = 9999;
-        String codeCompany = String.valueOf((int) Math.floor(Math.round((Math.random() * (max - min + 1) + min))));
-        System.out.println(codeCompany);
-        company.setCodeCompany(nameex + company.getAccount().getId() + codeCompany);
-        company.setStatusCompany(Status.HOT);
-        if (company.getAvatar() == null) {
-            return new ResponseEntity<>(new ResponseMessage("no_avatar_category"), HttpStatus.OK);
-        }
-        companyService.save(company);
-        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Company> deleteCompany(@PathVariable Long id) {
-        Optional<Company> companyOptional = companyService.findById(id);
-        if (!companyOptional.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        companyService.deleteById(id);
-        return new ResponseEntity<>(companyOptional.get(), HttpStatus.OK);
-    }
-
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> detailCompany(@PathVariable Long id) {
-//        Optional<Company> company = companyService.findById(id);
-//        if (!company.isPresent()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        return new ResponseEntity<>(company, HttpStatus.OK);
-//    }
-
-    @GetMapping("/findByStatus/{status}")
-    public ResponseEntity<?> findByStatus(@PathVariable Integer status) {
-        List<Company> companyList = companyService.findCompanyByStatus(status);
-        return new ResponseEntity<>(companyList, HttpStatus.OK);
-    }
-
     @PutMapping("/change_status")
     public ResponseEntity<?> updateCompany1(@PathVariable Long id, @RequestBody StatusRequest statusRequest) {
         Account account = userDetailServices.getCurrentUser();
@@ -200,6 +151,36 @@ public class CompanyController {
             companyService.save(company1.get());
         }
         return new ResponseEntity<>(new ResponseMessage("Yes"), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detailCompany(@PathVariable Long id) {
+        Optional<Company> company = companyService.findById(id);
+        if (!company.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(company, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCompany(@PathVariable Long id) {
+        Optional<Company> company = companyService.findById(id);
+        if (!company.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        companyService.deleteById(id);
+        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
+    }
+    @GetMapping("/findByStatus/{status}")
+    public ResponseEntity<?> findByStatus(@PathVariable Integer status){
+        List<Company> companyList = companyService.findCompanyByStatus(status);
+        return new ResponseEntity<>(companyList,HttpStatus.OK);
+    }
+
+    @GetMapping("/findByRecuitmentNewNeed")
+    public ResponseEntity<?> findByRecuitmentNewNeed(){
+        List<CompanyRecruitmentNeed> recruitmentNeedList = companyService.findCompanyByRecuitmentNew();
+        return new ResponseEntity<>(recruitmentNeedList,HttpStatus.OK);
     }
 
 }
